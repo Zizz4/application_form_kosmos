405cf7c02663b777d1f237fc97789df1b2118cee
kosmoswave <rizki@kosmoswave.com>
2023-07-18 11:37:27 +0700

    Application Form Kosmos

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Expense Form Kosmos',
+    'version': '16.0',
+    'summary': 'Application Form of Advance Fund, Lean and Reimbursement',
+    'description': 'Form for making Expense',
+    'category': 'Human Resources/Expenses',
+    'author': 'Muhamad Syahril Aziz',
+    'license': 'LGPL-3',
+    'depends': ['hr_expense'],
+    'data': [''],
+    'demo': [''],
+    'installable': True,
+    'auto_install': False
+}
