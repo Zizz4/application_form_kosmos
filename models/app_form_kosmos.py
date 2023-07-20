179732a71a912a3b441eba5aac51e60eb939a59e
kosmoswave <rizki@kosmoswave.com>
2023-07-20 15:00:31 +0700

    tambah date setiap status dan edit total-in-words

--- models/app_form_kosmos.py
+++ models/app_form_kosmos.py
@@ -1,7 +1,14 @@
 from odoo import fields, models, api
-
+from datetime import date
 
 class HrExpenseSheetAppFormKosmos(models.Model):
     _inherit = 'hr.expense.sheet'
 
     budget_code = fields.Char(string='Budget Code', stored=True)
+    submit_date = fields.Date(string='Submit Date', readonly=True)
+    approved_date = fields.Date(string='Approved Date', readonly=True)
+
+    def action_submit_sheet(self):
+        self.write({'state': 'submit',
+                    'submit_date': date.today()})
+        self.sudo().activity_update()
